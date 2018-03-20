04c52a7b68dcfb90f3af5fb8a3719b09307f8ea4
Preston Lee <preston.lee@prestonlee.com>
2018-03-20 00:43:21 -0400

    Cleaning up a bit for discussion in a few hours. :(

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pandoc -f markdown_mmd -t docx --toc -o build/paper.docx README.md
-#pandoc -f markdown_mmd -t epub --toc -o build/index.epub README.md
-#pandoc -f markdown_mmd -t html --toc -o build/index.html README.md
+#pandoc -f markdown_mmd -t epub --toc -o build/paper.epub README.md
+#pandoc -f markdown_mmd -t html --toc -o build/paper.html README.md
 #pandoc -f markdown_mmd -t pdf --toc -o build/paper.pdf README.md
